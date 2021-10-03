5ba12235920ecfaf090e15e802c0fbf66658ea0b
stevenkordonowy <stevenkordonowy1991@gmail.com>
2021-10-03 12:59:49 -0600

    more

--- gring.py
+++ gring.py
@@ -2,6 +2,7 @@
 
 import logging
 import time
+import base64
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
@@ -11,26 +12,30 @@ logging.basicConfig(level='DEBUG')
 
 scope = 'playlist-modify-public playlist-modify-private'
 
+def get_base64_encoded_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
 def main():
     # args = get_args()
     app_id = '68e489df7f1b4cef9d6a69cc8a0b649a'
     app_secret = 'b6407e24d4e343d189a9d61590226f2a'
-    redirect_uri = 'http://127.0.0.1:9090'
+    redirect_uri = 'http://localhost:8000'
 
     sp = spotipy.Spotify(
-        # auth_manager = SpotifyOAuth(
-        #     scope = scope,
-        #     client_secret = app_secret,
-        #     client_id = app_id,
-        #     redirect_uri = redirect_uri
-        # )
-
-        auth_manager = SpotifyClientCredentials(
-            # scope = scope,
+        auth_manager = SpotifyOAuth(
+            scope = scope,
             client_secret = app_secret,
-            client_id = app_id
-            # redirect_uri = redirect_uri
+            client_id = app_id,
+            redirect_uri = redirect_uri
         )
+
+        # auth_manager = SpotifyClientCredentials(
+        #     # scope = scope,
+        #     client_secret = app_secret,
+        #     client_id = app_id
+        #     # redirect_uri = redirect_uri
+        # )
     )
 
     test_playlist_id = '4gcOpHvPb7lTATlQXOISHy'
@@ -45,6 +50,11 @@ def main():
                 public = True,
                 collaborative = False,
                 description = 'Just playin')
+
+            # sp.playlist_upload_cover_image(
+            #     test_playlist_id,
+            #     get_base64_encoded_image('droff.jpg')
+            # )
             print('done with {}'.format(c))
             c += 1
         except Exception as e:
