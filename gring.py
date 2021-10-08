4bf8680ebcb8f3bb0294574b3230fda0c2961efc
stevenkordonowy <stevenkordonowy1991@gmail.com>
2021-10-08 14:43:43 -0600

    trying it out

--- gring.py
+++ gring.py
@@ -12,49 +12,89 @@ from flask import Flask, redirect, request, render_template
 import requests as req
 import threading
 import os
-from flask_caching import Cache
 
 logger = logging.getLogger('green-ring-fix')
 logging.basicConfig(level='DEBUG')
 
-scope = 'playlist-modify-public playlist-modify-private ugc-image-upload user-follow-read'
+scope = 'playlist-modify-public playlist-modify-private ugc-image-upload'
 
-app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.urandom(64)
-# app.config['SESSION_TYPE'] = 'filesystem'
-# app.config['SESSION_FILE_DIR'] = './.flask_session/'
-# Session(app)
+import typing as t
+class Flasky(Flask):
 
-# caches_folder = './.spotify_caches/'
-# if not os.path.exists(caches_folder):
-#     os.makedirs(caches_folder)
+    def __init__(
+        self,
+        import_name: str,
+        static_url_path: t.Optional[str] = None,
+        static_folder: t.Optional[str] = "static",
+        static_host: t.Optional[str] = None,
+        host_matching: bool = False,
+        subdomain_matching: bool = False,
+        template_folder: t.Optional[str] = "templates",
+        instance_path: t.Optional[str] = None,
+        instance_relative_config: bool = False,
+        root_path: t.Optional[str] = None,
+    ):
+        super().__init__(import_name,
+        static_url_path,
+         static_folder,
+         static_host,
+         host_matching,
+         subdomain_matching,
+         template_folder,
+        instance_path,
+         instance_relative_config,
+        root_path)
 
-# def session_cache_path():
-#     return caches_folder + session.get('uuid')
+        self.sp = ''
+        self.authd = False
+
+# os.environ['SPOTIPY_CLIENT_SECRET'] = 'b6407e24d4e343d189a9d61590226f2a'
+# os.environ['SPOTIPY_REDIRECT_URI'] = 'https://gring-fix.herokuapp.com'
+# oauth = SpotifyOAuth(
+#         scope = scope,
+#         open_browser=False
+#     )
+# token = util.prompt_for_user_token(scope)
+app = Flasky(__name__)
 
 @app.route("/")
-def get():
-    threading.Thread(target = wrapper).start()
-    return 'Hello Clarice'
-    # oauth = SpotifyOAuth(
-    #         scope = scope,
-    #         # client_secret = app_secret,
-    #         # client_id = app_id,
-    #         # redirect_uri = redirect_uri,
-    #         open_browser=False
-    #     )
+def get(stuff = None):
+    if not app.authd:
+        print('Need to auth')
+        return redirect('/token')
+    threading.Thread(target = wrapper, args=[]).start()
+    return str(app.authd)
 
-    # auth_url = oauth.get_authorize_url()
+# Raj:
+# * Need a confirmed email, remove from PhD and put into masters, need SID
+# * QCS: Ask about conflicting
 
-    # # t = 
-    # return render_template('my-form.html').replace('SHIT', auth_url)
+@app.route("/token")
+def token():
+    oauth = SpotifyOAuth(
+            scope = scope,
+            open_browser=False
+        )
 
-@app.route('/', methods=['POST'])
+    auth_url = oauth.get_authorize_url()
+    return render_template('my-form.html').replace('SHIT', auth_url)
+
+@app.route('/token', methods=['POST'])
 def post():
     print('Ya hit the post!')
     text = request.form['text']
-    threading.Thread(target = wrapper).start()
-    return 'All is well... for now....'
+    
+    # token = util.prompt_for_user_token(scope)
+    # oauth = SpotifyOAuth(
+    #         scope = scope,
+    #         open_browser=False
+    #     )
+    # t = oauth.cache_handler.get_cached_token()
+
+    oauth = LazyAuth(code = text)
+    app.sp = spotipy.Spotify(oauth_manager=oauth)
+    app.authd = True
+    return redirect('/')
 
 
 class LazyAuth(SpotifyOAuth):
@@ -72,83 +112,43 @@ class LazyAuth(SpotifyOAuth):
         return self.code
 
 
-def kickoff_stuff(text):
-    oauth = LazyAuth(
-            code = text,
-            scope = scope,
-            open_browser=False
-        )
-
-    sp = spotipy.Spotify(
-        auth_manager = oauth
-    )
-
-    test_playlist_id = '4gcOpHvPb7lTATlQXOISHy'
-
-    description = 'Let your mind wander with some Organic/Melodic House'
-
-    c = 0
-    while True:
-        name = 'FUCKYOU{}'.format(c)
-        try:
-            sp.playlist_change_details(
-                test_playlist_id,
-                name = name,
-                public = True,
-                collaborative = False,
-                description = description)
-
-            sp.playlist_upload_cover_image(
-                playlist_id = test_playlist_id,
-                image_b64 = load_binary('droff.jpg')
-            )
-            print('done with {}'.format(c))
-            c += 1
-        except Exception as e:
-            logger.error('RUH ROH\n{}'.format(e))
-
-        time.sleep(30)
-
-
 import pathlib
 
 def load_binary(filename):
     with open(next(pathlib.Path('.').glob('{}'.format(filename))), "rb") as img_file:
         return base64.b64encode(img_file.read())
 
-def update(duck):
-    username = 'stevenkordonowy1991@gmail.com'
+def update():
+    # username = 'stevenkordonowy1991@gmail.com'
     # test_playlist_id = '4gcOpHvPb7lTATlQXOISHy'
     test_playlist_id = '6IHTHqqxr7IOAfRkHIpFKd'
+    test_name = 'For real praxctice!'
+
+    # playlist_id = ''
+    # name = 'Drifting Off'
     description = 'Let your mind wander with some Organic/Melodic House'
 
-    token = util.prompt_for_user_token(username, scope)
-    sp = spotipy.Spotify(auth=token)
-
-    name = 'Dawg{}'.format(duck + 1)
     try:
-        sp.playlist_change_details(
+        app.sp.playlist_change_details(
             test_playlist_id,
-            name = name,
+            name = test_name,
             public = True,
             collaborative = False,
             description = description)
 
         logging.getLogger().setLevel('INFO')
-        sp.playlist_upload_cover_image(
-            playlist_id = test_playlist_id,
+        app.sp.playlist_upload_cover_image(
+            playlist_id = playlist_id,
             image_b64 = load_binary('droff.jpg')
         )
         logging.getLogger().setLevel('DEBUG')
     except Exception as e:
         logger.error('RUH ROH\n{}'.format(e))
 
-    duck += 1
 
 def wrapper():
-    duck = 1
-    update(duck)
-    schedule.every(60).seconds.do(update, duck)
+    update()
+    schedule.every(60).seconds.do(update)
 
     while True:
         schedule.run_pending()
@@ -156,17 +156,8 @@ def wrapper():
         continue 
 
 if __name__ == '__main__':
-    logger.debug('HELLO!!!')    
-    # f = '{}/{}'.format(pathlib.Path().resolve().as_uri(), 'ball.jpg')
-    # load_binary(f)
-    # app_id = '68e489df7f1b4cef9d6a69cc8a0b649a'
-    # app_secret = 'b6407e24d4e343d189a9d61590226f2a'
-    # redirect_uri = 'http://www.example.com'
-    # os.environ['SPOTIPY_CLIENT_SECRET'] = 'b6407e24d4e343d189a9d61590226f2a'
-    # os.environ['SPOTIPY_REDIRECT_URI'] = 'https://gring-fix.herokuapp.com'
-    # app.run()
-    p = pathlib.Path('.')
-    print(list(p.glob('**/*.jpg')))
-    wrapper()
+    logger.info('Trying for real!')
+
+    app.run()
 
     
